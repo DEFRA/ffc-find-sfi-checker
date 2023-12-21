a0f7ff102e416f875d5d3103cc6633d7121427f3
Mohammed Foysal <Foysal.Mohammed@defra.gov.uk>
2023-12-21 17:27:07 +0000

    Add google tag manager config

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,8 @@
 const config = {
   serviceUri: process.env.SERVICE_URI || 'http://localhost:3000',
   baseUrl: 'https://gov.uk',
-  version: '1.0.10'
+  version: '1.0.10',
+  googleTagManagerKey: process.env.GOOGLE_TAG_MANAGER_KEY
 }
 
 module.exports = config
