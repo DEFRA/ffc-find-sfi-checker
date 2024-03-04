79732107fdce6ac89d9376310ce09f643e4297ce
Foysal Mohammed <MohammedFoysal@users.noreply.github.com>
2024-03-04 14:58:57 +0000

    Add post release content updates (#33)
    Add post release content updates
    
    - Fix accessibility statement not specifying site retirement date
    - Fix service link on accessibility page is wrong
    - Fix print page warning text refers to the site as "Service" instead of "Filtering tool"
    - Fix cookie banner not returning user back to their previous page
    
    ---------
    
    Co-authored-by: Mohammed Foysal <Foysal.Mohammed@defra.gov.uk>

--- app/routes/cookies-page.js
+++ app/routes/cookies-page.js
@@ -1,7 +1,13 @@
-module.exports = {
-  method: 'GET',
-  path: '/cookies',
-  handler: (request, h) => {
-    return h.view('cookies-page')
+const { getCurrentPolicy } = require('../cookies')
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/cookies',
+    handler: (request, h) => {
+      const cookiesPolicy = getCurrentPolicy(request, h)
+
+      return h.view('cookies-page', { cookiesPolicy })
+    }
   }
-}
+]
